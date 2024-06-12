8eccf6eb68bf089c3fe2977c376c4dc97d50351d
Gustavo <150259441+GustxGrtz@users.noreply.github.com>
2024-06-11 21:22:07 -0300

    att foto funcionando database
    aaaa

---
+++ cafeteria/Connect.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Conexao{
+
+    
+    public static function conectar(){
+        
+        try{
+            $conn = new PDO("mysql:host=localhost;dbname=Cafeteria", 'root', 'positivo');
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return $conn;
+        }
+        catch(PDOException $erro)
+        {
+            echo "Conexão Falhou! => " . $erro->getMessage();
+            return null;
+        }    
+    }
+}
+
+
+
+    // $servidor = "localhost";
+	// $usuario = "root";
+	// $senha = "positivo";
+	// $dbname = "Cafeteria";
+	
+	// //Criar a conexao
+	// $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+?>
